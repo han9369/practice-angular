bf4d96e31b8a4edfcd0a0f08fbe80eddd2fa0995
Han <gaohan.henry@Gmail.com>
2016-03-22 22:46:34 -0700

    add

--- js/app.js
+++ js/app.js
@@ -6,27 +6,34 @@
 		this.products = gems;
 		this.totals = 0;
 
-		this.add = function(value){
-			this.totals += value;
-		};
-
-		this.remove = function(value){
-			this.totals -= value;
-			if(this.totals <= 0){
-				this.totals = 0;
+		this.add = function(value,index){
+			if(gems[index].canPurchase){
+				this.totals += value;
+				gems[index].canPurchase = false;
 			}
 		};
 
+		this.remove = function(value,index){
+			if(!gems[index].canPurchase){
+				this.totals -= value;
+				if(this.totals <= 0){
+					this.totals = 0;
+				}
+				gems[index].canPurchase = true;
+			}
+		};
 	});
 
 	var gems = [
 		{
+			num: 0,
 			name: 'product1',
 			price: 20.95,
 			imgs: 'img/1.png',
 			canPurchase: true
 		},
 		{
+			num:1,
 			name: 'product2',
 			price: 50.95,
 			imgs: 'img/2.png',
@@ -34,6 +41,7 @@
 
 		},
 		{
+			num:2,
 			name: 'product3',
 			price: 100.30,
 			imgs: 'img/3.png',
